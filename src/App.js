2b637e5fe2349e04e3592483bc3f4d25f246b9d5
afrah said <p.afrah3@gmail.com>
2024-04-23 18:03:43 +0300

    API update

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 import SearchIcon from "./search.svg";
 import MovieCard from "./MovieCard";
 // 5a7e3b84
-const API_URL='http://www.omdbapi.com/?i=tt3896198&apikey=bfd1869e';
+const API_URL='https://www.omdbapi.com/?i=tt3896198&apikey=bfd1869e';
 const movie1={
     "Title": "Italian Spiderman",
     "Year": "2007",
